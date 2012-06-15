4b8006765dfe8d195f46fb9b7b95fd04613c058c
Blake Bassett <blkbsstt@gmail.com>
2012-06-15 15:55:23 -0500

    Added CloneLink stuff

---
+++ clonelink/README.md
@@ -0,0 +1,6 @@
+CloneLink  
+Copyright (c) 2012 The Board of Trustees of The University of Alabama  
+All rights reserved.  
+
+$ ant dist  
+$ ./clonelink.sh
---
+++ clonelink/clonelink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LIB_DIR="`dirname $0`/dist/lib"
+OPTIONS="-Xms512m -Xmx2048m"
+CP=
+
+if [ ! -d ${LIB_DIR} ]; then
+    echo "Please run \`ant dist\` then try again."
+    exit
+fi
+
+java ${OPTIONS} -jar ${LIB_DIR}/clonelink.jar ${@:1}
