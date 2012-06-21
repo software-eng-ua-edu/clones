9f811fa13f8f9eb8ad513f59c89778cd2487939e
Colin C. Hemphill <colin@hemphill.us>
2012-06-21 16:47:11 -0500

    Added preliminary GUI shell.

--- novel/src/java/edu/ua/eng/software/novel/Novel.java
+++ novel/src/java/edu/ua/eng/software/novel/Novel.java
@@ -1,18 +1,28 @@
 /*
-* [The "New BSD" license]
-* Copyright (c) 2012 The Board of Trustees of The University of Alabama
-* All rights reserved.
-*
-* See LICENSE for details.
-*/
-package edu.ua.eng.software.Novel;
+ * [The "New BSD" license]
+ * Copyright (c) 2012 The Board of Trustees of The University of Alabama
+ * All rights reserved.
+ *
+ * See LICENSE for details.
+ */
+package edu.ua.eng.software.novel;
+
+import javax.swing.SwingUtilities;
 
 /**
-* @author      Nicholas A. Kraft <nkraft@cs.ua.edu>
-*/
+ * @author      Nicholas A. Kraft <nkraft@cs.ua.edu>
+ * @author      Colin C. Hemphill <colin@hemphill.us>
+ */
 public class Novel {
 
-	public static void main (String [] args) throws Exception {
-        ///TODO
-	}
+    public static void main (String [] args) throws Exception {
+
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run() {
+                NovelFrame ui = new NovelFrame();
+                ui.initUI();
+                ui.setVisible(true);
+            }
+        });
+    }
 }
---
+++ novel/src/java/edu/ua/eng/software/novel/NovelFrame.java
@@ -0,0 +1,56 @@
+/*
+ * [The "New BSD" license]
+ * Copyright (c) 2012 The Board of Trustees of The University of Alabama
+ * All rights reserved.
+ *
+ * See LICENSE for details.
+ */
+package edu.ua.eng.software.novel;
+
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ * @author      Colin C. Hemphill <colin@hemphill.us>
+ */
+
+public class NovelFrame extends JFrame {
+
+    public final void initUI() {
+
+        JMenuBar menu = new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
+        JMenu help = new JMenu("Help");
+        menu.add(file); menu.add(edit); menu.add(help);
+
+        JMenuItem fileImport = new JMenuItem("Import/Run");
+        JMenuItem filePrefs = new JMenuItem("Preferences");
+        JMenuItem fileExit = new JMenuItem("Exit");
+
+        JMenuItem editDif = new JMenuItem("Difference");
+        JMenuItem editFilter = new JMenuItem("Filter");
+
+        JMenuItem helpTut = new JMenuItem("Tutorials");
+        JMenuItem helpAbout = new JMenuItem("About");
+
+        file.add(fileImport); file.add(filePrefs); file.add(fileExit);
+        edit.add(editDif); edit.add(editFilter);
+        help.add(helpTut); help.add(helpAbout);
+        file.setMnemonic(KeyEvent.VK_F);
+        setJMenuBar(menu);
+
+        setDefaultLookAndFeelDecorated(true);
+        setTitle("N.o.V.E.L.");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        NovelPanel panel = new NovelPanel();
+        setContentPane(panel.createContentPane());
+    }
+}
