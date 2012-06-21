aa4804114037a7ce75d516a86e0d1ebc7893e62e
Conor Kirkman <conor.kirkman@gmail.com>
2012-06-21 16:45:19 -0500

    Localised fragments

---
+++ clonerank/src/java/edu/ua/eng/software/clonerank/CodeFragment.java
@@ -0,0 +1,18 @@
+package edu.ua.eng.software.novel;
+
+public class CodeFragment
+{
+    public CodeFragment(String filename, int startLine, int endLine) {
+        this.filename = filename;
+        this.startLine = startLine;
+        this.endLine = endLine;
+    }
+
+    public String getFilename() { return filename; }
+    public int getStartLine() { return startLine; }
+    public int getEndLine() { return endLine; }
+
+    private String filename;
+    private int startLine;
+    private int endLine;
+}
