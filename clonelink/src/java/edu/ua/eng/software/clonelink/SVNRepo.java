2f9393022fa5601fae11a6d5fb70665610475306
parodeghero <parodeghero@gmail.com>
2012-06-25 13:28:46 -0400

    changed tabs to spaces

--- clonelink/src/java/edu/ua/eng/software/clonelink/SVNRepo.java
+++ clonelink/src/java/edu/ua/eng/software/clonelink/SVNRepo.java
@@ -25,30 +25,28 @@ import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
  */
 public class SVNRepo {
     @SuppressWarnings("rawtypes")
-	public void crawl() {
+    public void crawl() {
         DAVRepositoryFactory.setup();
 
-		String url = "https://jhotdraw.svn.sourceforge.net/svnroot/jhotdraw";
-		long startingRevision = 0;
-		long endingRevision = -1;
-				
+        String url = "https://jhotdraw.svn.sourceforge.net/svnroot/jhotdraw";
+        long startingRevision = 0;
+        long endingRevision = -1;
+                
      try{
-		SVNRepository repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(url));
-		Collection logEnteries = repository.log(new String[] {""}, null, startingRevision, endingRevision, true, true);
-		Pattern pattern = Pattern.compile(".*BUG.*", Pattern.CASE_INSENSITIVE);
+        SVNRepository repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(url));
+        Collection logEnteries = repository.log(new String[] {""}, null, startingRevision, endingRevision, true, true);
+        Pattern pattern = Pattern.compile(".*BUG.*", Pattern.CASE_INSENSITIVE);
 
-		for(Iterator entries = logEnteries.iterator();entries.hasNext();){
-			SVNLogEntry logEntry = (SVNLogEntry)entries.next();				
-			Matcher matcher = pattern.matcher(logEntry.toString());
-				 
-			if(matcher.find()){
-				System.out.println("Message Log: " + logEntry.getMessage());
+        for(Iterator entries = logEnteries.iterator();entries.hasNext();){
+            SVNLogEntry logEntry = (SVNLogEntry)entries.next();             
+            Matcher matcher = pattern.matcher(logEntry.toString());
+                 
+            if(matcher.find()){
+                System.out.println("Message Log: " + logEntry.getMessage());
                         }
                 }
-        }	
+        }   
         catch (Exception e){
         }
-
-	}
-
+    }
 }
