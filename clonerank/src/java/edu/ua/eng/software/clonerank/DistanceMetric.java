8662f87203a27c7395cb32f9b37986d2ec3f908b
Blake Bassett <blkbsstt@gmail.com>
2012-06-25 16:35:21 -0500

    Preliminary distance metric code

--- clonerank/src/java/edu/ua/eng/software/clonerank/DistanceMetric.java
+++ clonerank/src/java/edu/ua/eng/software/clonerank/DistanceMetric.java
@@ -3,6 +3,7 @@ package edu.ua.eng.software.clonerank;
 import java.io.File;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Iterator;
 
 public abstract class DistanceMetric
 {
@@ -17,5 +18,21 @@ public abstract class DistanceMetric
             System.out.println("test");
         } while (file != null);
         return pathList;
+    }
+
+    protected int pathDistance(List<String> path1, List<String> path2) {
+        int distance = -1;
+        if(path1.get(0).equals(path2.get(0))) {
+            path1 = path1.subList(0, path1.size() - 1);
+            path2 = path2.subList(0, path2.size() - 1);
+
+            distance = path1.size() + path2.size();
+            Iterator<String> it1 = path1.iterator();
+            Iterator<String> it2 = path2.iterator();
+            while(it1.hasNext() && it2.hasNext() && it1.next().equals(it2.next())) {
+                distance -= 2;
+            }
+        }
+        return distance;
     }
 }
---
+++ clonerank/src/java/edu/ua/eng/software/clonerank/TLPDistanceMetric.java
@@ -0,0 +1,57 @@
+package edu.ua.eng.software.clonerank;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Allows for filesystem distance based on package naming conventions.
+ * 
+ *
+ */
+
+public class TLPDistanceMetric extends DistanceMetric
+{
+    public TLPDistanceMetric() {
+        for(String tlp: defaultTlps) tlpSet.add(tlp);
+    }
+
+    public TLPDistanceMetric(String tlp) {
+        this();
+        addTLP(tlp);
+    }
+
+    public void addTLP(String tlp) {
+        tlpSet.add(tlp);
+    }
+
+    public int getDistance(String path1, String path2) {
+        List<String> pathList1 = breakPath(path1);
+        List<String> pathList2 = breakPath(path2);
+        int index1 = findIndex(pathList1);
+        int index2 = findIndex(pathList2);
+        if(index1 != -1 && index2 != -1) {
+            pathList1 = pathList1.subList(index1, pathList1.size());
+            pathList2 = pathList2.subList(index2, pathList2.size());
+            return pathDistance(pathList1, pathList2);
+        } else {
+            //TODO: no top level package?
+        }
+        return -1;
+    }
+
+    protected int findIndex(List<String> path) {
+        ListIterator<String> it = path.listIterator();
+        while(it.hasNext()) {
+            String segment = it.next();
+            if(tlpSet.contains(segment)) {
+                return it.previousIndex();
+            }
+        }
+        return -1;
+    }
+
+    final Set<String> tlpSet = new HashSet<String>();
+    final String[] defaultTlps = {"edu", "com", "net", "org"};
+}
