05e73ebc5c013102b9c4368af6b1fb2e28cd48db
Blake Bassett <blkbsstt@gmail.com>
2012-06-26 13:31:03 -0500

    Fixed import of CodeFragment

--- clonerank/src/java/edu/ua/eng/software/clonerank/importing/NiCadHandler.java
+++ clonerank/src/java/edu/ua/eng/software/clonerank/importing/NiCadHandler.java
@@ -15,7 +15,7 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import edu.ua.eng.software.novel.CodeFragment;
+import edu.ua.eng.software.clonerank.CodeFragment;
 
 
 /**
