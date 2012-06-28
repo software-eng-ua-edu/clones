fd59c6c365b7f2ef2a93bee9ced3db935c20d353
Colin C. Hemphill <colin@hemphill.us>
2012-06-28 15:10:05 -0500

    Formatting.

--- novel/src/java/edu/ua/eng/software/novel/NovelSourceViewer.java
+++ novel/src/java/edu/ua/eng/software/novel/NovelSourceViewer.java
@@ -16,36 +16,38 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 /**
- * Creates the contents of the Source Viewer tab
+ * Creates the contents of the Source Viewer tab. This class uses the
+ * JSyntaxPane library to enable syntax highlighting in the source viewer.
  * 
  * @author Colin C. Hemphill <colin@hemphill.us>
  */
 
-public class NovelSourceViewer extends JPanel {
-	JEditorPane sourcePane;
-	JScrollPane sourceView;
+public class NovelSourceViewer extends JPanel
+{
+    JEditorPane sourcePane;
+    JScrollPane sourceView;
 
-	public NovelSourceViewer() {
-		super(new GridLayout(1, 0));
+    public NovelSourceViewer() {
+        super(new GridLayout(1, 0));
 
-		jsyntaxpane.DefaultSyntaxKit.initKit();
-		sourcePane = new JEditorPane();
-		sourcePane.setEditable(false);
-		sourceView = new JScrollPane(sourcePane);
-		sourcePane.setContentType("text/java");
-	}
+        jsyntaxpane.DefaultSyntaxKit.initKit();
+        sourcePane = new JEditorPane();
+        sourcePane.setEditable(false);
+        sourceView = new JScrollPane(sourcePane);
+        sourcePane.setContentType("text/java");
+    }
 
-	public JScrollPane blankSource() {
-		return sourceView;
-	}
+    public JScrollPane blankSource() {
+        return sourceView;
+    }
 
-	public JScrollPane setSource(String sourceFile) {
-		try {
-			FileReader fr = new FileReader(sourceFile);
-			sourcePane.read(fr, null);
-		} catch (IOException e) {
-			System.err.println(e);
-		}
-		return sourceView;
-	}
+    public JScrollPane setSource(String sourceFile) {
+        try {
+            FileReader fr = new FileReader(sourceFile);
+            sourcePane.read(fr, null);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return sourceView;
+    }
 }
