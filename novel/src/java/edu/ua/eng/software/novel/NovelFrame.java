1d628c082c0655d7e0aee5392ebef54f63472c06
Colin C. Hemphill <colin@hemphill.us>
2012-06-29 15:10:27 -0500

    Minor changes to the menus, shortcuts, and documentation.

--- novel/src/java/edu/ua/eng/software/novel/NovelFrame.java
+++ novel/src/java/edu/ua/eng/software/novel/NovelFrame.java
@@ -8,6 +8,7 @@
 package edu.ua.eng.software.novel;
 
 import java.awt.Desktop;
+import java.awt.Event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -23,6 +24,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Creates the main UI frame and menu
@@ -35,6 +37,7 @@ public class NovelFrame extends JFrame implements ActionListener
 
     public final void initUI() {
 
+        // top level menu items
         JMenuBar menu = new JMenuBar();
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
@@ -43,27 +46,34 @@ public class NovelFrame extends JFrame implements ActionListener
         menu.add(edit);
         menu.add(help);
 
+        // sub menu items
         JMenuItem fileImport = new JMenuItem("Import/Run");
-        fileImport.addActionListener(this);
-        fileImport.setActionCommand("Open");
         JMenuItem filePrefs = new JMenuItem("Preferences");
         JMenuItem fileExit = new JMenuItem("Exit");
-
         JMenuItem editDif = new JMenuItem("Difference");
         JMenuItem editFilter = new JMenuItem("Filter");
-
         JMenuItem helpTut = new JMenuItem("Tutorials");
         JMenuItem helpAbout = new JMenuItem("About");
 
-        helpTut.setActionCommand("Tutorial");
-        helpTut.setMnemonic(KeyEvent.VK_F1);
+        // set action commands
+        fileImport.setActionCommand("OPEN");
+        fileImport.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I,
+                Event.CTRL_MASK));
+        fileImport.addActionListener(this);
+        filePrefs.setActionCommand("PREFS");
+        filePrefs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P,
+                Event.CTRL_MASK));
+        filePrefs.addActionListener(this);
+        fileExit.setActionCommand("EXIT");
+        fileExit.addActionListener(this);
+        helpTut.setActionCommand("TUTORIAL");
         helpTut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
         helpTut.addActionListener(this);
-        helpAbout.setActionCommand("About");
-        helpAbout.setMnemonic(KeyEvent.VK_F2);
+        helpAbout.setActionCommand("ABOUT");
         helpAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F2, 0));
         helpAbout.addActionListener(this);
 
+        // add the sub menus
         file.add(fileImport);
         file.add(filePrefs);
         file.add(fileExit);
@@ -76,25 +86,58 @@ public class NovelFrame extends JFrame implements ActionListener
         setTitle("N.o.V.E.L.");
         setSize(1024, 768);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         panel = new NovelPanel();
         setContentPane(panel.createContentPane());
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Tutorial"))
+        if (e.getActionCommand().equals("OPEN"))
+            openDialog();
+        else if (e.getActionCommand().equals("PREFS"))
+            prefsDialog();
+        else if (e.getActionCommand().equals("EXIT"))
+            System.exit(0);
+        else if (e.getActionCommand().equals("TUTORIAL"))
             try {
-                createTutorial();
+                openTutorial();
             } catch (IOException e1) {
                 e1.printStackTrace();
             } catch (URISyntaxException e1) {
                 e1.printStackTrace();
             }
-        else if (e.getActionCommand().equals("About"))
+        else if (e.getActionCommand().equals("ABOUT"))
             createAboutMenu();
-        else if (e.getActionCommand().equals("Open"))
-            openDialog();
+    }
+
+    public void openDialog() {
+
+        final JFileChooser chooser = new JFileChooser(
+                System.getProperty("user.dir"));
+        chooser.setFileHidingEnabled(false);
+        chooser.setDialogTitle("Import/Run");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                ".java and .txt Files", "java", "txt");
+        chooser.setFileFilter(filter);
+
+        int returnVal = chooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            panel.updateStatus("Imported file " + file.getAbsolutePath());
+            panel.updateSource(file.toString());
+        }
+    }
+
+    public void prefsDialog() {
+
+        // will use java.util.prefs package to handle preference storage
+        // will most likely use a new "NovelPrefs" class to display
+    }
+
+    public void openTutorial() throws IOException, URISyntaxException {
+
+        Desktop.getDesktop().browse(new URI("about:blank"));
     }
 
     public void createAboutMenu() {
@@ -104,23 +147,5 @@ public class NovelFrame extends JFrame implements ActionListener
                 + "\nColin Hemphill, Conor Kirkman"
                 + "\nNicholas Kraft, Paige Rodeghero", "About N.o.V.E.L.",
                 JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    public void createTutorial() throws IOException, URISyntaxException {
-
-        Desktop.getDesktop().browse(new URI("about:blank"));
-    }
-
-    public void openDialog() {
-
-        final JFileChooser chooser = new JFileChooser(
-                System.getProperty("user.dir"));
-        int returnVal = chooser.showOpenDialog(this);
-        if (chooser.getSelectedFile() != null
-                && chooser.getSelectedFile().isFile()) {
-            File file = chooser.getSelectedFile();
-            panel.updateStatus("Imported file " + file.getAbsolutePath());
-            panel.updateSource(file.toString());
-        }
     }
 }
