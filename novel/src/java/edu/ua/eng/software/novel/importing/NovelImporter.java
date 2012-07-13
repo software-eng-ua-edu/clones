1a3d2f325508ead789906be45d8bc30263e52708
cmferris <cmferris1@crimson.ua.edu>
2012-07-13 16:12:33 -0500

    yep

---
+++ novel/src/java/edu/ua/eng/software/novel/importing/NovelImporter.java
@@ -0,0 +1,53 @@
+/*
+ * [The "New BSD" license]
+ * Copyright (c) 2012 The Board of Trustees of The University of Alabama
+ * All rights reserved.
+ *
+ * See LICENSE for details.
+ */
+package edu.ua.eng.software.novel;
+
+import java.io.File;
+import java.util.Vector;
+
+import de.uni_bremen.st.rcf.imports.Import;
+import de.uni_bremen.st.rcf.imports.Import.CloneFormat;
+import de.uni_bremen.st.rcf.model.RCF;
+
+/**
+ * For importing the various clone detection files
+ * 
+ * @author Casey Ferris <cmferris1@crimson.ua.edu>
+ * @author Blake Bassett <rbbassett@crimson.ua.edu>
+ */
+
+public class NovelImporter
+{
+    public static void importReport(File importFile, File sourceDir, ReportType type) {
+        RCF imported = Import.importData(type.toCloneFormat(), importFile.getAbsolutePath(), "test/test.rcf", sourceDir.getAbsolutePath());
+
+        CloneDataModel.importData(imported.getVersions().getFirstEntry());
+
+        NovelPanelController.getInstance().loadCloneData(); 
+    }
+
+    protected NovelImporter() {
+
+    }
+
+    public enum ReportType {
+        NICAD, RCF;
+
+        public CloneFormat toCloneFormat()
+        {
+            switch(this) {
+                case NICAD:
+                    return CloneFormat.NICAD;
+                case RCF:
+                    return CloneFormat.RCF;
+                default:
+                    return null;
+            }
+        }
+    }
+}
