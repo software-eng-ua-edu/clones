e926b9c48a0f92d2d78d38094bef163204172f76
parodeghero <parodeghero@gmail.com>
2012-07-13 17:36:51 -0400

    updated buglink tree

--- novel/src/java/edu/ua/eng/software/novel/BugLinkView.java
+++ novel/src/java/edu/ua/eng/software/novel/BugLinkView.java
@@ -7,12 +7,43 @@
  */
 package edu.ua.eng.software.novel;
 
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.JScrollPane;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
 /**
- * Creates the visible UI panel and initializes components
  * 
  * @author Paige A. Rodeghero <parodeghero@bsu.edu>
  */
-public class BugLinkView
+public class BugLinkView extends JPanel
 {
-	
+	private JTree bugTree;
+	private JScrollPane scrollPane;
+
+	public BugLinkView(){
+		bugTree = new JTree();
+		bugTree.setModel(createModel());
+
+		scrollPane = new JScrollPane();
+		scrollPane.add(bugTree);
+		add(scrollPane);
+
+		add(bugTree);
+	}
+
+	private DefaultTreeModel createModel(){
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Buggies");
+		DefaultMutableTreeNode category = null;
+		DefaultMutableTreeNode bug = null;
+		
+		category = new DefaultMutableTreeNode("Bug types");
+		root.add(category);
+
+		bug = new DefaultMutableTreeNode("This is a bug.  aka bug #1 or tech. bug #0...yeah");
+		category.add(bug);
+
+		return new DefaultTreeModel(root);
+	}
 }
