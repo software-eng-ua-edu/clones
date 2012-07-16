652c9b34236ee075a77efbd8fcdc73e8cff17637
parodeghero <parodeghero@gmail.com>
2012-07-16 16:48:09 -0400

    remove commented out code

--- novel/src/java/edu/ua/eng/software/novel/TreeRenderer.java
+++ novel/src/java/edu/ua/eng/software/novel/TreeRenderer.java
@@ -18,10 +18,8 @@ public class TreeRenderer extends DefaultTreeCellRenderer
 {
 	public Component getTreeCellRendererComponent(JTree tree, Object val, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus ){
 		super.getTreeCellRendererComponent(tree, val, sel, expanded, leaf, row, hasFocus);
-
-		//if(leaf){
-			this.setLeafIcon(null);
-		//}
+		
+		this.setLeafIcon(null);
 
 		return this;
 	}	
